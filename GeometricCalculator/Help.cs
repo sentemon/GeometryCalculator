ea79b9b57e239ac0d7f4cfb934e0b5cfe50c4494
Ivan Sentemon <sentemon.i@gmail.com>
2024-05-10 17:09:55 +0200

    Created Help class

---
+++ GeometricCalculator/Help.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace GeometricCalculator
+{
+    public class Help
+    {
+        public static void ShowHelp()
+        {
+            Console.WriteLine("Geometric Calculator");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("\t!help - Show this help");
+            Console.WriteLine("\t!exit - Exit the program");
+        }
+    }
+}
